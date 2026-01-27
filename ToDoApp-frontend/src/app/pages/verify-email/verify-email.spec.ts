241a9f2c9c492890345c1997eed50edf2f1d942f
ahmed-mhadhbi <ahmed.mhadhbil.75@gmail.com>
2026-01-27 15:54:51 +0100

    Fix Angular

--- ToDoApp-frontend/src/app/pages/verify-email/verify-email.spec.ts
+++ ToDoApp-frontend/src/app/pages/verify-email/verify-email.spec.ts
@@ -1,23 +1,22 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { VerifyEmail } from './verify-email';
+ import { VerifyEmailComponent } from './verify-email';
 
-// describe('VerifyEmail', () => {
-//   let component: VerifyEmail;
-//   let fixture: ComponentFixture<VerifyEmail>;
+ describe('VerifyEmail', () => {
+   let component: VerifyEmailComponent;
+   let fixture: ComponentFixture<VerifyEmailComponent>;
+   beforeEach(async () => {
+     await TestBed.configureTestingModule({
+       imports: [VerifyEmailComponent]
+     })
+     .compileComponents();
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [VerifyEmail]
-//     })
-//     .compileComponents();
+     fixture = TestBed.createComponent(VerifyEmailComponent);
+     component = fixture.componentInstance;
+     await fixture.whenStable();
+   });
 
-//     fixture = TestBed.createComponent(VerifyEmail);
-//     component = fixture.componentInstance;
-//     await fixture.whenStable();
-//   });
-
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+   it('should create', () => {
+     expect(component).toBeTruthy();
+  });
+ });
